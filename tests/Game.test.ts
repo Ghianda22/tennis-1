4d71812be28a08b26bd47dae7be434bbe7ff3ac2
Valentina Lorè <valentina.lore@soprasteria.com>
2023-06-14 15:13:03 +0200

    Modified test list

--- tests/Game.test.ts
+++ tests/Game.test.ts
@@ -1,9 +1,9 @@
 /** TEST LIST
+ *  [-] When a player who won less than three points wins a point, the score should be updated
+ *  [-] Given a player with a score of 40, if they win the next point and have a lead of at least two points, they should win the game
  *  [-] Given a game with at least three points scored by each player, when the scores are equal the game score should be "deuce"
  *  [-] Given a game in deuce, when a player win a point the game score should be "advantage" for that player
  *  [-] Given a game score of "advantage", if the player with advantage wins the next point they should win the game
  *  [-] Given a game score of "advantage", if the player without advantage wins the next point, game score should be back at "deuce"
- *  [-] Given a player with a score of 40, if they win the next point and have a lead of at least two points, they should win the game
- *  [-]
  *  [-]
  * */
